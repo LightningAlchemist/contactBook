10cbeb0850a822dd1ed2540fa6193d061d4131da
LightningAlchemist <joseph.casebeer@gmail.com>
2017-12-11 19:36:02 +0900

    Add files via upload

---
+++ Contact_Book_Classes.java
@@ -0,0 +1,17 @@
+
+public class Contact_Book_Classes {
+	private static void Alphabetical_Order(){
+		
+	}
+	public static void Start_Menu(){
+		System.out.println("Welcome to Joey's Contact Book");
+		System.out.println("What would you like to do?");
+		System.out.println();
+		System.out.println("  1: Add Contacts");
+		System.out.println("  2: Delete Contacts");
+		System.out.println("  3: Edit Contacts");
+		System.out.println("  4: Sort Contacts");
+		System.out.println("  5: Search Contacts");
+		System.out.println("  6: Close Contact Book");
+	}
+}
---
+++ Contact_Book_Main.java
@@ -0,0 +1,9 @@
+
+public class Contact_Book_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Contact_Book_Classes.Start_Menu();
+	}
+
+}
